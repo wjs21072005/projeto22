7150e8b74c710a05e86e69f0c344647457ac512b
wjs21072005 <wagnerjustinianosantana@gmail.com>
2023-06-21 11:38:44 -0300

    Criação de tela de Cadastro
    Criação da tela de Cadastro já
    gravando no banco tratando validações
    do usuario ao cadastrar Criação da pagina HOME do sistema Web

---
+++ conectadb.php
@@ -0,0 +1,17 @@
+<?php
+    ## ARQUIVO DE ACESSO AO BANCO DE DADOS 
+    ## ALERTA DE MODO PROFISSIONAL ARQUIVO DEVE-SE MANTER OCULTO E PROTEGIDO ##
+
+    ## LOCALIZA O SERVIDOR COM O BANCO (NOME DO COMPUTADOR)
+    $servidor = "localhost:3307";
+    ## NOME DO BANCO
+    $banco = "projeto22";
+    ##USUARIO DE ACESSO
+    $usuario = "administrador";
+    ##SENHA DO USUARIO
+    $senha = "123";
+
+  #LINK DE CONEXÃO
+  $link = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+?>
---
+++ login.php
@@ -0,0 +1,58 @@
+<?php
+#ABRE UMA VARIAVEL SESSÃO
+session_start();
+
+#SOLICITA O ARQUIVO
+include("conectadb.php");
+
+#EVENTO,APÓS CLICK NO BOTÃO LOGAR
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+     $nome = $_POST['nome'];
+     $senha = $_POST['senha'];
+
+     #QUERY DE BANCO DE DADOS
+     $sql = "SELECT COUNT(usu_id) FROM usuarios WHERE usu_nome = '$nome' 
+     AND usu_senha = '$senha'";
+     $retorno = mysqli_query($link, $sql);
+
+     #TODO RETORNO DO BANCO É RETORNADO EM ARRAY EM PHP
+     while($tbl = mysqli_fetch_array($retorno)) {
+          $cont = $tbl[0];
+     }
+     #VERIFICA SE USUARIO EXISTE
+     #SE $CONT == 1 ELE EXISTE E FAZ LOGIN
+     #SE $CONT == 0 ELE NAO EXISTE E USUARIO NAO ESTA CADASTRADO 
+     if ($cont == 1){
+          $sql = "SELECT * FROM usuarios WHERE usu_nome = '$nome' 
+          AND usu_senha = '$senha' AND usu_ativo = 's'";
+          #DIRECIONA USUARIO PARA ADM
+          echo"<script>window.location.href='admhome.php';</script>";
+      } 
+     else {
+          
+          echo"<script>window.alert('USUARIO OU SENHA INCORRETO');</script>";
+     }
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="./css/estiloadm.css">
+     <title>LOGIN USUARIO</title>
+</head>
+<body>
+     <form action="login.php" method="post">
+          <h1>LOGIN DE USUARIO</h1>
+          <input type="text" name="nome" placeholder="NOME">
+          <p></p>
+          <input type="password" name="senha" placeholder="SENHA">
+          <p></p>
+          <input type="submit" name="login" placeholder="LOGIN">
+     </form>
+
+
+</body>
+</html>
